4690ee7f5432b4838209e8680c422ea39bb8ab2c
gorann1 <gorandevop@outlook.com>
2023-05-22 18:12:05 +0200

    #Configure database atributes_2_before locations

--- assets/App.vue
+++ assets/App.vue
@@ -10,7 +10,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
             <nav>
                 <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
+                <RouterLink to="/about">Projekt</RouterLink>
                 <RouterLink to="/lokacije">Lokacije</RouterLink>
                 <RouterLink to="/centri">Centri</RouterLink>
             </nav>
---
+++ src/Entity/Visibility.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiResource;
+use App\Repository\VisibilityRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+
+#[ORM\Entity(repositoryClass: VisibilityRepository::class)]
+#[ApiResource]
+class Visibility
+{
+    use TimestampableEntity;
+    
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    /**
+     * @var string|null
+     *
+     * @Gedmo\Slug(fields={"name"})
+     * @ORM\Column(length=128, unique=true)
+     */
+    #[ORM\Column(length: 128, unique: true)]
+    #[Gedmo\Slug(fields: ['name'])]
+    private $slug;
+
+    /**
+     * @var \DateTime $created
+     *
+     * Gedmo\Timestampable(on="create")
+     * @ORM\Column(type="datetime")
+     */
+    private $created;
+
+    /**
+     * @var \DateTime $updated
+     *
+     * Gedmo\Timestampable(on="update")
+     * @ORM\Column(type="datetime")
+     */
+    private $updated;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    public function getUpdated()
+    {
+        return $this->updated;
+    }
+}
